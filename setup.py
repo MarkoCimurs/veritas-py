ad6661df0f4e6f484e2dd6b20aecaab8010954d3
Marko Cimurs <cimursmarko@gmail.com>
2024-01-19 20:28:38 +0200

    added basic project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='veritas',
+    version='0.1',
+    packages=find_packages(),
+    description='A property-based testing library for Python.',
+    keywords='testing property-based-testing python',
+)
